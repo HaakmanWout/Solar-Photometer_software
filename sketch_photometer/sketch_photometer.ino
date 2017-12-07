a578d499617d2e1b18512cd0bd117152f955703d
HaakmanWout <wouthaakman@hotmail.com>
2017-12-07 21:49:33 +0100

    Solar Photometer LCD screen module Tick counter

--- sketch_photometer/sketch_photometer.ino
+++ sketch_photometer/sketch_photometer.ino
@@ -1,9 +1,17 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <LiquidCrystal.h>
 
+LiquidCrystal lcd(11, 10, 5, 4, 3, 2);
+
+void setup() {
+  pinMode(12, HIGH);
+  digitalWrite(12, LOW );
+
+  lcd.begin(16, 2);
+lcd.print("Zonfotometer");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  lcd.setCursor(0, 1);
+  lcd.print(millis() / 1000);
 }
+
