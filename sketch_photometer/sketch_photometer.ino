ec8699472583fb61a9f9207ed1d8f381f18e210f
HaakmanWout <wouthaakman@hotmail.com>
2017-12-08 20:31:50 +0100

    Final Solar Photometer proj

--- sketch_photometer/sketch_photometer.ino
+++ sketch_photometer/sketch_photometer.ino
@@ -1,17 +1,25 @@
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(11, 10, 5, 4, 3, 2);
+const int rs = 11, en = 10, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-void setup() {
+int photocellPin = 0;
+int photocellReading;
+
+void setup() {  
   pinMode(12, HIGH);
   digitalWrite(12, LOW );
 
   lcd.begin(16, 2);
-lcd.print("Zonfotometer");
+  lcd.print("Zonfotometer");
 }
 
 void loop() {
+  photocellReading = analogRead(photocellPin);
+  
   lcd.setCursor(0, 1);
-  lcd.print(millis() / 1000);
+  lcd.print(photocellReading);
+
+  delay(100);
 }
 
